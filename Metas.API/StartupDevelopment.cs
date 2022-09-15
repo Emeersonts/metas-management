dd3b375dab7d0e4073b8a60dbb424179dac7cba5
Emerson Teixeira de Souza <emerson.souza@ex.gerdau.com>
2022-09-15 16:18:56 -0300

    Atualização Startup de Desenvolvimento

---
+++ Metas.API/StartupDevelopment.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metas.API
+{
+    public class StartupDevelopment :Startup
+    {
+        public StartupDevelopment(IConfiguration configuration) :base (configuration)
+        {
+            
+        }
+
+        
+    }
+}
