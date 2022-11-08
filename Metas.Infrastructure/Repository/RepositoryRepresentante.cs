e0a8ce3adcaedf7d62987b52fba5066eb18a451a
Emerson Teixeira de Souza <emerson.souza@ex.gerdau.com>
2022-11-08 14:49:56 -0300

    Erro chache

--- Metas.Infrastructure/Repository/RepositoryRepresentante.cs
+++ Metas.Infrastructure/Repository/RepositoryRepresentante.cs
@@ -21,7 +21,7 @@ namespace Metas.Infrastructure.Repository
 
             parametro[cont] = new SqlParameter("@PR_TIPO", SqlDbType.Int);
             parametro[cont].Direction = ParameterDirection.Input;
-            parametro[cont].Value = pkxd.type;
+            parametro[cont].Value = 0;
 
             cont++;
             parametro[cont] = new SqlParameter("@PR_IDFUNCIONALIDADE", SqlDbType.Int);
