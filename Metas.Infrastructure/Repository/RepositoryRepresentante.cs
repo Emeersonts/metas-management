0e7998cc2bdedcf0a12f8f0ba6941f43c91b17af
Emerson Teixeira de Souza <emerson.souza@ex.gerdau.com>
2022-12-02 14:57:07 -0300

    QA V1

--- Metas.Infrastructure/Repository/RepositoryRepresentante.cs
+++ Metas.Infrastructure/Repository/RepositoryRepresentante.cs
@@ -529,7 +529,7 @@ namespace Metas.Infrastructure.Repository
 
             parametro[cont] = new SqlParameter("@PR_TIPO", SqlDbType.Int);
             parametro[cont].Direction = ParameterDirection.Input;
-            parametro[cont].Value = pkxd.type;
+            parametro[cont].Value = 1;
 
             cont++;
             parametro[cont] = new SqlParameter("@PR_IDFUNCIONALIDADE", SqlDbType.Int);
